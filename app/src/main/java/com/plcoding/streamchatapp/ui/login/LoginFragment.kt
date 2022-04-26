c8598b9e99fe255c25beee5605e171d19e82df71
Genielson <ge.nielson@hotmail.com>
2022-04-26 19:35:56 -0300

    Adiciona class LoginFragment

---
+++ app/src/main/java/com/plcoding/streamchatapp/ui/login/LoginFragment.kt
@@ -0,0 +1,13 @@
+package com.plcoding.streamchatapp.ui.login
+
+import android.view.LayoutInflater
+import androidx.viewbinding.ViewBinding
+import com.plcoding.streamchatapp.databinding.FragmentLoginBinding
+import com.plcoding.streamchatapp.ui.BindingFragment
+
+class LoginFragment : BindingFragment<FragmentLoginBinding>() {
+
+    override val bindingInflater: (LayoutInflater) -> ViewBinding
+        get() = TODO("Not yet implemented")
+
+}
